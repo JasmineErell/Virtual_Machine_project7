1f8a191dc80b69542e8a2f7c534b8e49bd2f366d
JasmineErell <jasmine.erell@gmail.com>
2024-12-10 18:18:30 +0200

    fixed the commits for brtter debugging

--- CodeWriter.py
+++ CodeWriter.py
@@ -87,33 +87,28 @@ class CodeWriter:
                 f.write("M=!M\n")  # NOT x
     def WritePushPop(self, command, segment, index):
         with open(self.out_path, "a", newline='\n') as f:
+            f.write("//" + command + " " + segment + " " + index + "\n")
             if command == "push":
                 ## Push operations takes the value stored in RAM and push it into SP
                 if segment == "constant":
-                    f.write("// D = " + index + "\n")
                     f.write("@" + index + "\n")
                     f.write("D=A\n")
-                    f.write("// RAM[SP] = " + index + "\n")
                     f.write("@SP\n")
                     f.write("A=M\n")
                     f.write("M=D\n")
-                    f.write("//SP++\n")
                     f.write("@SP\n")
                     f.write("M=M+1\n")
 
                 elif segment == "pointer":
                     if index == 0: ##Push pointer 0
-                        f.write("//Access the THIS register (RAM[3])\n")
                         f.write("@THIS\n")
                         f.write("D=M\n")
                         f.write("@SP\n")
                         f.write("A=M\n")
                         f.write("M=D\n")
-                        f.write("//SP ++\n")
                         f.write("@SP\n")
                         f.write("M=M+1\n")
                     elif index == 1: ##Push pointer 1
-                        f.write("//Access the THIS register (RAM[3])\n")
                         f.write("@THAT\n")
                         f.write("D=M\n")
                         f.write("@SP\n")
@@ -125,19 +120,14 @@ class CodeWriter:
 
                 else:
                     ##Segments witch are not const or pointer - takes the value @X from the table
-                    f.write("//Load the index " + str(index) + "\n")
                     f.write("@" + str(index) + "\n")
                     f.write("D=A\n")
-                    f.write("//Base address of the " + segment + " segment\n")
                     f.write("@" + self.segment_table.get(segment) + "\n")
                     f.write("A=M+D\n")
-                    f.write("//Insert into D the value of " + self.segment_table.get(segment) + " " + str(index) + "\n")
                     f.write("D=M\n")
-                    f.write("//RAM[SP] = D\n")
                     f.write("@SP\n")
                     f.write("A=M\n")
                     f.write("M=D\n")
-                    f.write("//SP ++\n")
                     f.write("@SP\n")
                     f.write("M=M+1\n")
 
@@ -145,14 +135,12 @@ class CodeWriter:
                 ##Pop commands removes a value from the head of stack, and store it in RAM
                 if segment == "pointer":
                     if index == 0:
-                        f.write("//Access the SP, store its value in @THIS and decrement SP by 1\n")
                         f.write("@SP\n")
                         f.write("AM=M-1\n")
                         f.write("D=M\n")
                         f.write("@THIS\n")
                         f.write("M=D\n")
                     if index == 1:
-                        f.write("//Access the SP, store its value in @THAT and decrement SP by 1\n")
                         f.write("@SP\n")
                         f.write("AM=M-1\n")
                         f.write("D=M\n")
@@ -160,20 +148,15 @@ class CodeWriter:
                         f.write("M=D\n")
                 else:
                     #Poping commands that are not pointers
-                    f.write("//Compute the address " + self.segment_table.get(segment) + "+ " + str(index) + "and store it in R13 temporally\n")
                     f.write("@" + self.segment_table.get(segment) + "\n") #@segment
                     f.write("D=M\n")
                     f.write("@" + str(index) + "\n") #@index
                     f.write("D=D+A\n") #D= segment+index
-                    f.write("//Store the relevant address inside R13\n")
                     f.write("@R13\n")
                     f.write("M=D\n")
-                    f.write("//Pop the top of the stack:\n")
-                    f.write("//First we store in D the value of SP and decrement SP by 1\n")
                     f.write("@SP\n")
                     f.write("AM=M-1\n")
                     f.write("D=M\n")
-                    f.write("//Second we take the popped value and store it in the wanted address\n")
                     f.write("@R13\n")
                     f.write("A=M\n")
                     f.write("M=D\n")
